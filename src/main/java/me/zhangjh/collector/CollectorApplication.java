fcb622d7865455d3ad0dd824353645208a705628
zhangjh <njhxzhangjihong@126.com>
2022-09-01 01:02:51 +0800

    revert

--- src/main/java/me/zhangjh/collector/CollectorApplication.java
+++ src/main/java/me/zhangjh/collector/CollectorApplication.java
@@ -2,11 +2,8 @@ package me.zhangjh.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {
-        "classpath:test.properties"}, encoding = "UTF-8")
 public class CollectorApplication {
 
     public static void main(String[] args) {
