1c557d3b34bf4e03b1303c406f88f9a6d60b932d
zhangjh <njhxzhangjihong@126.com>
2022-09-01 00:11:46 +0800

    解决配置文件中文乱码问题

--- src/main/java/me/zhangjh/collector/CollectorApplication.java
+++ src/main/java/me/zhangjh/collector/CollectorApplication.java
@@ -2,11 +2,8 @@ package me.zhangjh.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {
-        "classpath:application.properties"}, encoding = "UTF-8")
 public class CollectorApplication {
 
     public static void main(String[] args) {
