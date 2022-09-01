4e5a39f5ed495d13bb5055df1850f1034950a5c6
zhangjh <njhxzhangjihong@126.com>
2022-09-01 23:47:56 +0800

    fix

--- start.sh
+++ start.sh
@@ -30,7 +30,7 @@ if [ $? -ne 0 ];then
 fi
 
 ## fatjar启动
-if [ "$1" == "package" ];then
+if [ "X$1" == "Xpackage" ];then
     mvn clean package -Dmaven.test.skip=true
 elseif
     nohup mvn spring-boot:run &
