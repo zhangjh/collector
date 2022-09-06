f9d3c66dc0adfb80b4eaa9d12b7222d1db05e0d1
zhangjh <njhxzhangjihong@126.com>
2022-09-06 12:13:43 +0800

    默认不下载字幕弹幕

--- src/main/download.sh
+++ src/main/download.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-## 下载前已经通过start.sh保证了you-get的安装，程序里也保证了参数1是下载路径，参数2是待下载的正确url
+## 下载前已经通过start.sh保证了you-get的安装，程序里也保证了参数1是下载路径，参数2是待下载的正确url，参数3随便填，填了代表需要下载字幕、弹幕等，默认不下
 if [ ! -d $1 ];then
     mkdir -p $1
 fi
-you-get --playlist --no-caption -o $1 $2
+if [ $3 -ne '' ];then
+    you-get --playlist -o $1 $2
+else
+    you-get --playlist --no-caption -o $1 $2
+fi
