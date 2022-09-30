025c6c0f8da2a539a4b0f0fb33ad3beda8153b9c
njhxzhangjh <njhxzhangjihong@126.com>
2022-09-30 09:17:45 +0800

    安装chromium

--- start.sh
+++ start.sh
@@ -22,11 +22,11 @@ if [ $? -ne 0 ];then
     docker run --name redis-collector -p 6379:6379 -d redis redis-server --save 60 1 --loglevel warning --requirepass redis_passwd
 fi
 
-## 下载安装chromedriver驱动
-#which chromedriver
-#if [ $? -ne 0 ];then
-#    apt -y install chromium-chromedriver
-#fi
+## 下载安装chrome
+which chromium
+if [ $? -ne 0 ];then
+    snap install chromium
+fi
 
 ## 安装mvn
 which mvn
